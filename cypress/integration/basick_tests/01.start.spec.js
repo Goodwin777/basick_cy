841ba5ecf5cc31a4048bda48fb5d976609d15f33
OleksiiHavrylov <oleksii.havrylov.ua@remotal.io>
2020-12-22 00:00:46 +0200

    Added first test

--- cypress/integration/basick_tests/01.start.spec.js
+++ cypress/integration/basick_tests/01.start.spec.js
@@ -1,13 +1,10 @@
- 
-const myUrl = Cypress.config().baseUrl 
-before(() => { cy.visit(myUrl);
-});
+ const myUrl = Cypress.config().baseUrl
+ before(() => {
+   cy.visit(myUrl);
+ });
 
-describe('My First Test', () => {
-    it('Visits the Kitchen Sink', () => {
-      cy.log('hello');
-    })
-    it('Visits the Kitchen Sink', () => {
-        cy.log('world');
-      })
-  })
+ describe('My First Test', () => {
+   it('Visits the Kitchen Sink', () => {
+     cy.log('hello');
+   })
+ })
