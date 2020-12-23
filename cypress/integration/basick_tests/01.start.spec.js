1680889242f3d144256e877d5a937946a196014f
OleksiiHavrylov <oleksii.havrylov.ua@remotal.io>
2020-12-23 19:45:02 +0200

    Find text on site

--- cypress/integration/basick_tests/01.start.spec.js
+++ cypress/integration/basick_tests/01.start.spec.js
@@ -1,10 +1,27 @@
- const myUrl = Cypress.config().baseUrl
- before(() => {
-   cy.visit(myUrl);
- });
+//  const myUrl = Cypress.config().baseUrl
+before(() => {
+  cy.visit('/');
+});
 
- describe('My First Test', () => {
-   it('Visits the Kitchen Sink', () => {
-     cy.log('hello');
-   })
- })
+describe('My First Test', () => {
+  it('Check the title of page', () => {
+
+    cy.title().should('eq', 'Google')
+
+
+  });
+
+  it('get text of first element', () => {
+    const eventNames = []
+
+
+    cy.get('.gb_g').eq(0)
+      .each(($el) => {
+         cy.wrap($el.text())
+ 
+
+
+      });
+    // cy.log(eventNames)
+  });
+});
